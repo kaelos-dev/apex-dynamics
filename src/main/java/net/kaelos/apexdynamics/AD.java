cc6d4f65d232d01388254cd8e3df04520c7130f3
LondonDerri <londi.jdev@gmail.com>
2026-08-20 23:04:33 +0300

    first commit

---
+++ src/main/java/net/kaelos/apexdynamics/AD.java
@@ -0,0 +1,11 @@
+package net.kaelos.apexdynamics;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(AD.MOD_ID)
+public class AD {
+    public static final String MOD_ID = "ad";
+
+    public AD(final IEventBus eventBus) {}
+}
